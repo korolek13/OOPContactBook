6da59458673b11b1501a4d6f45ec039076e71c4f
korolek13 <75901516+korolek13@users.noreply.github.com>
2020-12-12 18:58:15 +0300

    changes

--- OOP.py
+++ OOP.py
@@ -4,14 +4,22 @@ class Contact:
         self.phone = phone
         self.birthday = birthday
         print('Создан новый контакт')
-
+    
+    def show_info(self):
+        print(f'{self.name} - phone: {self.phone}, birthday: {self.birthday}')
+        
+    def __str__(self):
+        return self.name
+        
 Mike = Contact('Mihail Bulgakov', '20327', '15.05.1891')
 Vlad = Contact('Vladimir Mayakovsky', '7388', '19.07.1893')
+Max = Contact('Maksim Savelyev', '7388', '19.07.1893')
 
-def print_contact():
-    print(f'{Mike.name} - phone: {Mike.phone}, birthday: {Mike.birthday}')
-    print(f'{Vlad.name} - phone: {Vlad.phone}, birthday: {Vlad.birthday}')
 
 Mike.phone = '27'
 
-print_contact()
+Mike.show_info()
+Vlad.show_info()
+Max.show_info()
+
+print(Max)
